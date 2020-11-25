6ec4d5ac04077d065c788079682a3a4cb76182a9
Denis <dineresc@gmail.com>
2020-11-24 23:12:58 -0300

    up project

---
+++ src/main/java/com/dnc/kafkademo/kafkademo/NewOrderMain.java
@@ -0,0 +1,35 @@
+package com.dnc.kafkademo.kafkademo;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var key = "akey";
+        var value = "123443,1333,13431243215";
+        var producer = new KafkaProducer<String, String>(properties());
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+        producer.send(record, (res, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("topic: " + res.topic() + " :: partition: " + res.partition() + " :: offset: " + res.offset());
+        } ).get();
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return properties;
+    }
+}
